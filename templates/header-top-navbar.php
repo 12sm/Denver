616d3bdf23fb6121779e5b527a34f042cf7c7591
Ian Noyes <ian@12southmusic.com>
2014-07-02 18:21:15 -0500

    different data-toggle

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -4,7 +4,7 @@
 }
 ?>
 
-<header class="banner navbar navbar-default navbar-static-top" role="banner" data-spy="affix" data-offset-top="300">
+<header class="banner navbar navbar-default navbar-static-top" role="banner" data-spy="affix" data-offset-top="552">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
