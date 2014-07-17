961d3a1b8f1cbdd29d6966861e517af34a174b10
Ian Noyes <ian@12southmusic.com>
2014-07-17 14:03:25 -0500

    main-wrap selector

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ $('a[href*=#]:not([href=#])').click(function() {
       });
 
 //OneScroll
-$(".main").onepage_scroll({
+$(".main-wrap").onepage_scroll({
    sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
    easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", 
                                     // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
