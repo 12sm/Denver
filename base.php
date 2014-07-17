5acc78ce641c7d36ccee4fcf3723e7492af8eecc
Ian Noyes <ian@12southmusic.com>
2014-07-17 14:03:01 -0500

    main-wrap

--- base.php
+++ base.php
@@ -6,7 +6,9 @@
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
     </div>
   <![endif]-->
-
+<?php if (is_front_page) {
+  echo "<div class='main-wrap'>";
+} ?>
   <?php
     do_action('get_header');
     // Use Bootstrap's navbar if enabled in config.php
@@ -31,6 +33,8 @@
   </div><!-- /.wrap -->
 
   <?php get_template_part('templates/footer'); ?>
-
+<<?php if (is_front_page) {
+  echo "</div>";
+} ?>
 </body>
 </html>
