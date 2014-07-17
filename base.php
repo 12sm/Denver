65436d6b6ad93d38c35c2d3b7ad6cf4b317da3fb
Ian Noyes <ian@12southmusic.com>
2014-07-17 16:26:20 -0500

    blamo

--- base.php
+++ base.php
@@ -15,7 +15,7 @@
       get_template_part('templates/header');
     }
   ?>
-<?php if (!is_front_page()) {
+<?php if (!is_front_page()) { ?>
   <div class="wrap container-fluid" role="document">
     <div class="content row">
       <main class="main <?php echo roots_main_class(); ?>" role="main">
@@ -29,6 +29,6 @@
     </div><!-- /.content -->
   </div><!-- /.wrap -->
   <?php get_template_part('templates/footer'); ?>
- } ?>
+ <?php } ?>
 </body>
 </html>
