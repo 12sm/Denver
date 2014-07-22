7ebdfdb35cdc20b433896d7e502112264aef4e02
Ian Noyes <ian@12southmusic.com>
2014-07-22 13:54:45 -0500

    about template

---
+++ templates/content-page-about.php
@@ -0,0 +1,9 @@
+<div class='row'>
+	<div class="col-sm-8">
+		<img src='/new/image/stuff' class='img-responsive' />
+	</div>
+	<div class='col-sm-4'>
+	<h1>ABOUT</h1>
+	<?php the_content(); ?>
+	</div>
+</div>
