bd71bba1ac9df9ca40b336bd25836ce778e9800f
Ian Noyes <ian@12southmusic.com>
2014-07-22 15:07:34 -0500

    homepage styles

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -13,7 +13,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="/assets/img/logo.png" class="img-responsive"></a></a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
