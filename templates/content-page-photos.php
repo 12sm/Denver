79172c0869225b8b3ed7d6de3d848ca3a987ef15
Ian Noyes <ian@12southmusic.com>
2014-07-22 12:09:58 -0500

    put gal listing in photos page

--- templates/content-page-photos.php
+++ templates/content-page-photos.php
@@ -1,1 +1,10 @@
-<?php echo do_shortcode("[wpv-view name='PHOTOS | GALLERIES']"); ?>
+<div class="row">
+	<div class="col-sm-12">
+		<?php echo do_shortcode('[wpv-view name="PHOTOS | GALLERY LINKS"]'); ?>
+	</div>
+</div>
+<div class="row">
+	<div class="col-sm-12">
+		<?php echo do_shortcode("[wpv-view name='PHOTOS | GALLERIES']"); ?>
+	</div>
+</div>
