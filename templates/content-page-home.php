2368a923b0357baabc21074dd14fe71b5ead7d2b
Ian Noyes <ian@12southmusic.com>
2014-07-23 10:48:58 -0500

    delete clone brand

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -13,7 +13,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="/assets/img/logo.png" class="img-responsive"></a></a>
+ 	<a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="/assets/img/logo.png" class="img-responsive"></a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
