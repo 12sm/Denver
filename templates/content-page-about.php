94e75031067d7d9056ea8a96f6043222c3e39fc2
Ian Noyes <ian@12southmusic.com>
2014-07-23 13:01:40 -0500

    more about page stuff

--- templates/content-page-about.php
+++ templates/content-page-about.php
@@ -1,6 +1,5 @@
-<div class='row'>
-	<div class="col-sm-7 about-bg">
-		<img src='/assets/img/' class='img-responsive' />
+<div class='row about-bg'>
+	<div class="col-sm-7 ">
 	</div>
 	<div class='col-sm-5'>
 	<h1>ABOUT</h1>
