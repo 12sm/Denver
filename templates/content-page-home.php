95e08a44b4ecbbbeb6b8081bcf071e0611d9a385
Ian Noyes <ian@12southmusic.com>
2014-07-23 11:55:03 -0500

    views and about

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -41,7 +41,7 @@
 			</div>
 			<div class="col-sm-6">
 				<div class="about-wrap">
-					<h1>Who are <span class='huge'>We?</span></h1>
+					<h1 class='about'>Who are <span class='huge'>We?</span></h1>
 						<?php echo do_shortcode('[about]'); ?>
 						<a href="/about/" class="btn btn-about">Click to Find Out More </a>
 				</div>
