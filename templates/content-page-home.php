1e91c11b8c30555e840568529d5034e8edd61cc7
Ian Noyes <ian@12southmusic.com>
2014-07-24 12:34:42 -0500

    Revert "this?"
    This reverts commit d410f3e54a3ecbe91af4a520dd14210256c0737c.

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,4 +1,4 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
+<header class="banner navbar navbar-default navbar-static-top" role="banner" data-spy="affix" data-offset-top="0">
   <div class="container-fluid">
     <div class="col">
     <div class="navbar-header">
