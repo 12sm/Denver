e58df6033130b1cf8f6942d4b025f4c5013439bc
Ian Noyes <ian@12southmusic.com>
2014-07-24 11:19:45 -0500

    try this

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -28,6 +28,7 @@
 </header>
 </section>
 <section id='about'>
+<img src="/wp-content/themes/denver/assets/img/denver.png" class='about-denver'>
  <div class="wrap container-fluid" role="document">
     <div class="content row">
       <main class="main <?php echo roots_main_class(); ?>" role="main">
@@ -36,7 +37,6 @@
 				<div class="about-imgs-container">
 					<img src="/wp-content/themes/denver/assets/img/trumpet.png" class='about-trumpet'>
 					<img src="/wp-content/themes/denver/assets/img/guitar.png" class='about-guitar'>
-					<img src="/wp-content/themes/denver/assets/img/denver.png" class='about-denver'>
 				</div>
 			</div>
 			<div class="col-sm-6">
