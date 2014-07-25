5278e0bb26d8f52e603e970775784fe01177599b
Ian Noyes <ian@12southmusic.com>
2014-07-25 14:12:53 -0500

    .mobile-denver

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -9,6 +9,8 @@
       <h1>Denver &</h1>
       <h2>The Mile High</h2>
       <h2>Orchestra</h2>
+      <img src="/wp-content/themes/denver/assets/img/denver.png" class='mobile-denver'>
+
   </main>
 </div>
 </section>
