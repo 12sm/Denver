4c37e305629c3ee688b0eb36f86b6ac2e9b1ce15
Sarah <Sarah@12southmusic.com>
2014-08-28 17:51:21 -0500

    single page layout bootstrapping

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,11 +1,15 @@
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
-  </article>
+  <div class="row">
+  	<div class="col-sm-12 col-xs-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3"
+		  <article <?php post_class(); ?>>
+			<div class="entry-content">
+			  <?php the_content(); ?>
+			</div>
+			<footer>
+			  <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+			</footer>
+			<?php comments_template('/templates/comments.php'); ?>
+		  </article>
+		</div>
+	</div>
 <?php endwhile; ?>
