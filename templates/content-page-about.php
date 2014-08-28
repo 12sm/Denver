4601d7ae4796f4147dab2077085531d16fd3075b
Sarah <Sarah@12southmusic.com>
2014-08-28 17:18:34 -0500

    about page col's

--- templates/content-page-about.php
+++ templates/content-page-about.php
@@ -1,7 +1,7 @@
 <div class='row about-bg'>
-	<div class="col-sm-7 ">
+	<div class="col-sm-7 col-md-6">
 	</div>
-	<div class='col-sm-5'>
+	<div class="col-sm-5 col-md-6">
 	<h1>ABOUT</h1>
 	<?php the_content(); ?>
 	</div>
