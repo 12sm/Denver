40fb25cd46acc483effc79f272b8e43eed244bfb
Sarah <Sarah@12southmusic.com>
2014-08-29 15:56:21 -0500

    nvm

--- templates/content-single.php
+++ templates/content-single.php
@@ -3,7 +3,6 @@
   	<div class="col-sm-12 col-xs-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2"
 		  <article <?php post_class(); ?>>
 			<div class="entry-content">
-			  <?php echo get_the_post_thumbnail(); ?> 
 			  <?php the_content(); ?>
 			</div>
 			<footer>
