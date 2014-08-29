0e121eb570757f7af1819dade49f59289a4f52ec
Sarah <Sarah@12southmusic.com>
2014-08-29 16:06:42 -0500

    nother col!

--- templates/content-page-photos.php
+++ templates/content-page-photos.php
@@ -3,7 +3,7 @@
 		<?php echo do_shortcode('[wpv-view name="PHOTOS | GALLERY LINKS"]'); ?>
 	</div>
 </div>
-<div class="row">
+<div class="row col">
 	<div class="col-sm-12">
 		<?php echo do_shortcode("[wpv-view name='PHOTOS | GALLERIES']"); ?>
 	</div>
