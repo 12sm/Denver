d01ba95ce811721a678780e56c971457490eda48
Sarah <Sarah@12southmusic.com>
2014-08-29 15:59:54 -0500

    moving up top

--- templates/content-single.php
+++ templates/content-single.php
@@ -3,8 +3,8 @@
   	<div class="col-sm-12 col-xs-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2"
 		  <article <?php post_class(); ?>>
 			<div class="entry-content">
-			  <?php the_content(); ?>
 			  <?php echo get_the_post_thumbnail( $post_id, 'large' );  ?> 
+			  <?php the_content(); ?>
 			</div>
 			<footer>
 			  <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
