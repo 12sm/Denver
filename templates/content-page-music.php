ed72344d5ca04cb6f37e6d71bfe0cb56e9183515
Sarah <Sarah@12southmusic.com>
2014-09-05 15:31:05 -0500

    music page cols

--- templates/content-page-music.php
+++ templates/content-page-music.php
@@ -1,5 +1,5 @@
 <div class="row col">
-  	<div class="col-sm-12 col-xs-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+  	<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
 		<?php echo do_shortcode("[wpv-view name='ALBUM | MUSIC-PAGE-FEATURE']"); ?>
 	</div>
 </div>
