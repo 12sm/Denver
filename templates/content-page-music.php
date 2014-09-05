b9acdd80105776191ea7008ab7f87e01d3ea84f5
Ian Noyes <ian@12southmusic.com>
2014-09-05 12:37:44 -0500

    offset for music page

--- templates/content-page-music.php
+++ templates/content-page-music.php
@@ -1,1 +1,5 @@
-<?php echo do_shortcode("[wpv-view name='ALBUM | MUSIC-PAGE-FEATURE']"); ?>
+<div class="row col">
+  	<div class="col-sm-12 col-xs-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+		<?php echo do_shortcode("[wpv-view name='ALBUM | MUSIC-PAGE-FEATURE']"); ?>
+	</div>
+</div>
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,10 +1,10 @@
 <?php while (have_posts()) : the_post(); ?>
   <div class="row col">
-  	<div class="col-sm-12 col-xs-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2"
+  	<div class="col-sm-12 col-xs-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
 		  <article <?php post_class(); ?>>
 			<div class="entry-content">
 			  <?php the_content(); ?>
-			  <?php echo get_the_post_thumbnail( 'large' );  ?> 
+			  <?php echo get_the_post_thumbnail( 'large' );  ?>
 			</div>
 			<footer>
 			  <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
