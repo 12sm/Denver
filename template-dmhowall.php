9034ba38165c83144fbaf5f31d7ac8b0ad05b2c4
Sarah <Sarah@12southmusic.com>
2014-09-25 14:52:16 -0500

    dmho wall template

---
+++ template-dmhowall.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Template Name: DMHO Wall of Fame Template
+*/
+?>
+
+<?php get_template_part('templates/content', 'page-dmhowall'); ?>
---
+++ templates/content-page-dmhowall.php
@@ -0,0 +1,8 @@
+<div class='row about-bg'>
+	<div class="col-sm-6 col-md-6">
+	</div>
+	<div class="col-sm-6 col-md-6">
+	<h1>ABOUT</h1>
+	<?php the_content(); ?>
+	</div>
+</div>
