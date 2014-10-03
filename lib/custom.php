67270b7aa717ab38c58f36afd82904ab0621be8e
michael <michael@12southmusic.com>
2014-10-03 14:10:40 -0500

    eventdate shortcode

--- lib/custom.php
+++ lib/custom.php
@@ -1,5 +1,13 @@
 <?php 
 
+
+// Add Shortcode
+function event_date() {
+	echo tribe_event_format_date( false, 'D. M j, Y' );
+}
+add_shortcode( 'eventdate', 'event_date' );
+
+
 define( 'UPLOADS', ''.'assets' );
 
 
