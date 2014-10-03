51a5f4a7d829f3a94d0b8ef97fd6ab9403545be1
michael <michael@12southmusic.com>
2014-10-03 14:54:49 -0500

    More tour stuff

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -83,10 +83,10 @@
     <div class="content row full-height">
       	<main class="main <?php echo roots_main_class(); ?> full-height" role="main">
 			<div class='row tour-home full-height'>
-				<div class="col-md-6 col-sm-12">
+				<div class="col-md-6 col-sm-12 full-height">
 				<img src='/wp-content/themes/denver/assets/img/People.png' class="img-responsive people">
 				</div>
-				<div class="col-md-6 col-sm-12">
+				<div class="col-md-6 col-sm-12 tour-dates">
 				<h1>Tour</h1>
 					<div><?php echo do_shortcode('[wpv-view name="HOME | TOUR"]'); ?></div>
 				</div>
