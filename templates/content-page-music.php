6c31b1e2b84cf38a2b075c63069e2d16cb67a9cf
Ian Noyes <ian@12southmusic.com>
2014-10-06 10:49:02 -0500

    new col

--- templates/content-page-music.php
+++ templates/content-page-music.php
@@ -1,5 +1,7 @@
-<div class="row col">
-  	<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
-		<?php echo do_shortcode("[wpv-view name='ALBUM | MUSIC-PAGE-FEATURE']"); ?>
+<div class="row">
+	<div class="col">
+	  	<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
+			<?php echo do_shortcode("[wpv-view name='ALBUM | MUSIC-PAGE-FEATURE']"); ?>
+		</div>
 	</div>
 </div>
