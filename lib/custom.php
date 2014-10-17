d415403face2980fd2a9ad1bc4d684b908930803
Ian Noyes <ian@12southmusic.com>
2014-10-17 12:03:57 -0500

    state shortcode

--- lib/custom.php
+++ lib/custom.php
@@ -16,6 +16,21 @@ return substr($eventtime, 5, 5 );
 }
 add_shortcode( 'eventdate', 'event_date' );
 
+// Add Shortcode
+function event_state( $atts ) {
+
+  // Attributes
+  extract( shortcode_atts(
+    array(
+      'id' => 'null',
+    ), $atts )
+  );
+
+  // Code
+return tribe_get_stateprovine($id);
+}
+add_shortcode( 'eventstate', 'event_state' );
+
 
 define( 'UPLOADS', ''.'assets' );
 
