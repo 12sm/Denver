7de82c5c8a6a2b25ca466631d461ca96af5aed28
Ian Noyes <ian@12southmusic.com>
2014-10-17 14:46:13 -0500

    oh god this is jank as hell

--- templates/content-single.php
+++ templates/content-single.php
@@ -2,12 +2,13 @@
   <div class="row">
   	<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
 		  <article <?php post_class(); ?>>
-			<div class="row">
 				<div class="entry-content">
 				  <?php the_content(); ?>
 				  <?php echo get_the_post_thumbnail( 'large' );  ?>
 				</div>
 			</article>
+			</div>
+		</div>
 			<footer>
 			  <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
 			</footer>
