442f6f62e0eca59b7d5749e2cba927ba028b40f5
Ian Noyes <ian@12southmusic.com>
2014-10-20 12:27:19 -0500

    just call one scroll;

--- assets/js/main.js
+++ assets/js/main.js
@@ -230,6 +230,7 @@ function theSetup(){
       oneScroll();
     };
 }
+oneScroll();
 
 enquire.register("only screen and (max-width : 992px), only screen and (max-width : 992px) and (orientation : landscape), (orientation : landscape) and (min-device-width : 768px) and (max-device-width : 1024px)", {
 
