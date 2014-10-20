7fd3239c29868a3b25cc66fbd6802e452e1b33f5
Ian Noyes <ian@12southmusic.com>
2014-10-20 14:02:36 -0500

    dis

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -6,8 +6,7 @@
 <div class="wrap container-fluid mobile-top" role="document">
     <div class="content row">
       <main class="main <?php echo roots_main_class(); ?>" role="main">
-      <h1>Denver & The Mile High</h1>
-      <h2>Orchestra</h2>
+      <h1>Denver & The Mile High Orchestra</h1>
       <img src="/wp-content/themes/denver/assets/img/denver.png" class='mobile-denver'>
   </main>
 </div>
