e5714b96f8b28c59e0ca66bbe9b612e296c755dd
Ian Noyes <ian@12southmusic.com>
2014-10-20 13:33:52 -0500

    try it meow

--- assets/js/main.js
+++ assets/js/main.js
@@ -230,7 +230,7 @@ function theSetup(){
     };
 }
 
-enquire.register("only screen and (max-width : 992px)", {
+enquire.register("only screen and (max-width : 992px), only screen and (max-width : 992px) and (orientation : landscape), (orientation : landscape) and (min-device-width : 768px) and (max-device-width : 1024px)", {
 
     match : function() {
       matchFunc();
