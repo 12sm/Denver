e0b87d9eb2215bb71007f8ea6ae11fc917a71d34
Ian Noyes <ian@12southmusic.com>
2014-10-21 17:39:08 -0500

    click

--- assets/js/main.js
+++ assets/js/main.js
@@ -351,7 +351,7 @@ enquire.register("only screen and (max-width : 992px), (orientation : landscape)
 
   store: {
     init: function() {
-      $('a[href="#sheet-music"]').on('shown.bs.tab', function () {
+      $('a[href="#sheet-music"]').on('click', function () {
         alert('tabbed to sheet music');
     soundManager.setup({
   // disable or enable debug output
