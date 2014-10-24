e1bba308eb8bc501d06894e16e638d1e4e7488da
Ian Noyes <ian@12southmusic.com>
2014-10-23 23:43:30 -0500

    tooltips and resized buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -285,6 +285,7 @@ enquire.register("only screen and (max-width : 992px), (orientation : landscape)
 
   single_albums: {
       init: function() {
+        $('.buy-sheet').tooltip(options)
         $('.menu-news').removeClass('active');
         $('.entry-content').removeClass('col-sm-6 col-sm-offset-3').addClass('col');
 
@@ -323,6 +324,7 @@ enquire.register("only screen and (max-width : 992px), (orientation : landscape)
 
   music: {
       init: function() {
+        $('.buy-sheet').tooltip(options)
         $('.menu-news').removeClass('active');
       soundManager.setup({
     // disable or enable debug output
