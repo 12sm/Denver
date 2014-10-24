8a96297bfd4ab0e20e322ab4f76b0416d1cd202b
Ian Noyes <ian@12southmusic.com>
2014-10-24 17:57:16 -0500

    footer cols

--- templates/footer.php
+++ templates/footer.php
@@ -4,12 +4,17 @@
       <div class="row sidebar-footer">
       	<?php dynamic_sidebar('sidebar-footer'); ?>
     </div>
-    <div class="row footer-nav">  
+    <div class="row footer-nav">
+    <div class="col-sm-6">
         <?php
-     if (has_nav_menu('footer_navigation')) :
-       wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => ''));
-     endif;
-  ?>
+           if (has_nav_menu('footer_navigation')) :
+             wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => ''));
+           endif;
+        ?>
+      </div>
+      <div class="col-sm-6">
+        <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
+      </div>
     </div>
          
       <div id="social" class="row social-nav">
