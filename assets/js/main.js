f226c2a72fccda49606115ff7786c0504ca00002
Ian Noyes <ian@12southmusic.com>
2014-10-26 22:32:23 -0500

    wrap images in a tag

--- assets/js/main.js
+++ assets/js/main.js
@@ -376,6 +376,14 @@ enquire.register("only screen and (max-width : 992px)", {
 
   post_type_archive_product: {
     init: function() {
+
+      $(".sheet-img").each(function() {
+        var $this = $(this);
+        var src = $this.attr('src');
+        $this.addClass('image');
+        var a = $('<a/>').attr('href', src);
+        $this.wrap(a);
+
       function sheetImg(){
         $('sheet-img').swipebox();
       }
