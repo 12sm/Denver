53faf22883c7e80019215bedc02e8367c0b001ee
michael <michael@12southmusic.com>
2014-10-28 20:31:16 -0500

    commerce updates

--- templates/content-page-sheet.php
+++ templates/content-page-sheet.php
@@ -1,12 +1,13 @@
 <div class="col">
+	<div class="page-header">
+		<h1>Sheet Music</h1>
+	</div>
 	<div class="col-md-12">
 		<div class="row">
 			<div class="col-md-6 col-sm-12">
 				<?php echo do_shortcode('[wpv-view name="STORE-MENU"]'); ?>
 			</div>
 		</div>
-		<div class="row">
 			<?php echo do_shortcode('[wpv-view name="Sheet Music"]'); ?>
-		</div>
 	</div>
 </div>
