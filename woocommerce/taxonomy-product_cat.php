95d09bf429bdff9aa933a5b99d9dd3c31a08abb2
michael <michael@12southmusic.com>
2014-10-28 21:26:49 -0500

    better cat

--- woocommerce/taxonomy-product_cat.php
+++ woocommerce/taxonomy-product_cat.php
@@ -17,4 +17,18 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 
 
-Hello Category!
+<div class="col">
+<div class="page-header">
+  <h1>
+    <?php echo roots_title(); ?>
+  </h1>
+</div>
+	<div class="col-md-12">
+		<div class="row">
+			<div class="col-md-12">
+				<?php echo do_shortcode('[wpv-view name="STORE-MENU"]'); ?>
+			</div>
+		</div>
+			<?php echo do_shortcode('[wpv-view name="Sheet Music"]'); ?>
+	</div>
+</div>
