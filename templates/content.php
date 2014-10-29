e43920d1bc057d77708bc071737fddc317a8b2f8
michael <michael@12southmusic.com>
2014-10-28 21:43:22 -0500

    wrong template

--- templates/content.php
+++ templates/content.php
@@ -1,5 +1,3 @@
-<div class="row col">
-<div class="col-md-12">
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -9,5 +7,3 @@
     <?php the_excerpt(); ?>
   </div>
 </article>
-</div>
-</div>
--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,8 +1,9 @@
 <?php while (have_posts()) : the_post(); ?>
+<div class="row col">
+<div class="col-md-12">
   <article <?php post_class(); ?>>
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
@@ -10,7 +11,8 @@
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
   </article>
+</div>
+</div>
 <?php endwhile; ?>
 
