d7f8333ee185c29c77a3e0e5050056689a26e5fe
Ian Noyes <ian@12southmusic.com>
2014-10-29 15:01:48 -0500

    try this

--- lib/custom.php
+++ lib/custom.php
@@ -1,40 +1,5 @@
 <?php 
 
-/**
- * Optimize WooCommerce Scripts
- * Remove WooCommerce Generator tag, styles, and scripts from non WooCommerce pages.
- */
-add_action( 'wp_enqueue_scripts', 'child_manage_woocommerce_styles', 99 );
- 
-function child_manage_woocommerce_styles() {
-  //remove generator meta tag
-  remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
- 
-  //first check that woo exists to prevent fatal errors
-  if ( function_exists( 'is_woocommerce' ) ) {
-    //dequeue scripts and styles
-    if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() && ! is_shop() && ! is_product_category() && ! is_product() && ! is_account_page() ) {
-      echo "deque is firing";
-      wp_dequeue_style( 'woocommerce_frontend_styles' );
-      wp_dequeue_style( 'woocommerce_fancybox_styles' );
-      wp_dequeue_style( 'woocommerce_chosen_styles' );
-      wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
-      wp_dequeue_script( 'wc_price_slider' );
-      wp_dequeue_script( 'wc-single-product' );
-      wp_dequeue_script( 'wc-add-to-cart' );
-      wp_dequeue_script( 'wc-add-to-cart-variation' );
-      wp_dequeue_script( 'wc-single-product' );
-      wp_dequeue_script( 'wc-chosen' );
-      wp_dequeue_script( 'prettyPhoto' );
-      wp_dequeue_script( 'prettyPhoto-init' );
-      wp_dequeue_script( 'jquery-blockui' );
-      wp_dequeue_script( 'jquery-placeholder' );
-      wp_dequeue_script( 'fancybox' );
-      wp_dequeue_script( 'jqueryui' );
-    }
-  }
- 
-}
 
 function posts_per_pagesize( $query ) {
 
