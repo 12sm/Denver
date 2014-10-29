847a74c5a704a8af6a29eeecfa3359d5e498dc61
michael <michael@12southmusic.com>
2014-10-28 21:40:32 -0500

    Let's fix some product single

--- templates/content.php
+++ templates/content.php
@@ -1,3 +1,5 @@
+<div class="row col">
+<div class="col-md-12">
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -7,3 +9,5 @@
     <?php the_excerpt(); ?>
   </div>
 </article>
+</div>
+</div>
