ecd94589ccdd1bf7f0d016b81fee9071de2af95d
michael <michael@12southmusic.com>
2014-10-28 22:11:27 -0500

    single page

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,8 @@
+<div class="row col">
+<div class="col-md-12">
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
+</div>
+</div>
