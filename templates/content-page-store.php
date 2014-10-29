9db3b56c1f5fa6c6253c2c9c54fbbbb4cb726251
michael <michael@12southmusic.com>
2014-10-28 20:12:53 -0500

    New store menu

--- templates/content-page-store.php
+++ templates/content-page-store.php
@@ -1,1 +1,12 @@
-<?php echo do_shortcode('[wpv-view name="STORE-MUSIC"]'); ?>
+<div class="col">
+	<div class="col-md-12">
+		<div class="row">
+			<div class="col-md-6 col-sm-12">
+				<?php echo do_shortcode('[wpv-view name="STORE-MENU"]'); ?>
+			</div>
+		</div>
+		<div class="row">
+			<?php echo do_shortcode('[wpv-view name="STORE-MUSIC"]'); ?>
+		</div>
+	</div>
+</div>
